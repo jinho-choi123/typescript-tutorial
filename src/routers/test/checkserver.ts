55181b423af02b8222ead54fa2f28ae5377b34c4
jinho-choi123 <choijinho817@gmail.com>
2023-01-29 13:36:26 +0000

    add testcase for currenttimestamp

--- src/routers/test/checkserver.ts
+++ src/routers/test/checkserver.ts
@@ -1,9 +1,10 @@
 import express, { Router, Request, Response } from "express";
+import currentTimestamp from "../../utils/currentTimestamp";
 
 var checkserverRouter: Router = express.Router();
 
 checkserverRouter.get('/', (req: Request, res: Response) => {
-    res.send(`Server is running! Current timestamp is ${Date.now()}`);
+    res.send(`Server is running! Current timestamp is ${currentTimestamp()}`);
 })
 
 export default checkserverRouter
