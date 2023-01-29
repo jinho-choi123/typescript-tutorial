efaab34e69b98078fdda2be77ee91b39f5b280cd
jinho-choi123 <choijinho817@gmail.com>
2023-01-29 21:19:30 +0000

    simplify to promise

--- src/db/user/create.ts
+++ src/db/user/create.ts
@@ -10,6 +10,8 @@ const createUser = (userEmail: String, userPassword1: String, userPassword2: Str
 
     //db에서 중복되는 user가 존재하는지 확인한다.
     //그리고 auth container에 요청하여, user add를 한다. request가 성공적이면, mongodb에 add한다.
+
+    
     return new Promise((resolve, reject) => {
         db()
             .then((conn) => {
