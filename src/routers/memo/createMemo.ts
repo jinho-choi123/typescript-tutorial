53c7d05219d9ad0e42928e2d37a740b1dfe42793
jinho-choi123 <choijinho817@gmail.com>
2023-01-30 07:22:53 +0000

    change creatememo module import name

--- src/routers/memo/createMemo.ts
+++ src/routers/memo/createMemo.ts
@@ -1,12 +1,12 @@
 import express, { Router, Request, Response } from "express";
-import create from "../../db/memo/create";
+import createMemo from "../../db/memo/create";
 import {Types} from 'mongoose';
 
 const createMemoRouter = (req: Request, res: Response) => {
     const userId = new Types.ObjectId(req.body.userId);
     const content = req.body.content;
 
-    create(userId, content)
+    createMemo(userId, content)
         .then((doc) => {
             res.send("created memo!")
         })
